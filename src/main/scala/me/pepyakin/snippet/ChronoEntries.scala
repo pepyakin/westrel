e5cc1afa7f3cc13e2d8fee3734350081b26a0d91
Sergey Pepyakin <frightinside@gmail.com>
2013-03-15 00:56:38 +0400

    Добавлен подсчет итогового баланса в списке доходов/расходов.

--- src/main/scala/me/pepyakin/snippet/ChronoEntries.scala
+++ src/main/scala/me/pepyakin/snippet/ChronoEntries.scala
@@ -7,15 +7,16 @@ import java.util.Date
 import Helpers._
 
 import me.pepyakin.lib.DependencyFactory
-import me.pepyakin.model.{AccountEntry, WestrelSchema, User}
+import me.pepyakin.model._
 import me.pepyakin.util.Auth
 
 /**
  * 
  * @author Sergey
  */
-object ChronoEntries {
+object ChronoEntries extends Loggable {
 
+  import AccountOp._
   import WestrelSchema._
   import org.squeryl.PrimitiveTypeMode._
 
@@ -26,18 +27,38 @@ object ChronoEntries {
   }
 
   def renderEntries(in: Seq[AccountEntry]) = {
-    import me.pepyakin.model.AccountOp._
+    renderData(in) & renderTotal(in)
+  }
 
-    "tr" #> in.map { entry =>
-      "@amount *" #> entry.amount &
-      "@date *" #> entry.date.toString &
-      "@category *" #> entry.category &
-      "tr [class]" #> {
-        entry.op match {
-          case INCOME => "success"
-          case OUTCOME => "error"
-        }
-      }
+  def renderTotal(in: Seq[AccountEntry]) = {
+    val (income, outcome) = in.partition(_.op == INCOME)
+
+    logger.info("income: " + income)
+    logger.info("outcome: " + outcome)
+
+    val totalIncome = income.map(_.amount).reduce(_ + _)
+    val totalOutcome = outcome.map(_.amount).reduce(_ - _)
+    val total = totalIncome - totalOutcome
+
+    logger.info("total: " + total)
+
+    "tr .total" #> {
+      "td .total *" #> total
+    }
+  }
+
+  def renderData(in: Seq[AccountEntry]) = {
+    "tr .data" #> in.map {
+      entry =>
+        "@amount *" #> entry.amount &
+          "@date *" #> entry.date.toString &
+          "@category *" #> entry.category &
+          "tr [class]" #> {
+            entry.op match {
+              case INCOME => "success"
+              case OUTCOME => "error"
+            }
+          }
     }
   }
 }
